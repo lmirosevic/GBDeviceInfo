d30251d63aeb3e3feaaa5f6436832d3dddc011f3
Luka Mirosevic <luka@goonbee.com>
2012-11-12 10:20:53 +0100

    applied Apache 2 license

--- GBDeviceInfo/GBDeviceInfo.m
+++ GBDeviceInfo/GBDeviceInfo.m
@@ -2,14 +2,20 @@
 //  GBDeviceInfo.m
 //  GBDeviceInfo
 //
-//  Created by Luka Mirosevic on 16/10/2012.
-//  Copyright (c) 2012 Luka Mirosevic. All rights reserved.
+//  Created by Luka Mirosevic on 11/10/2012.
+//  Copyright (c) 2012 Luka Mirosevic.
 //
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
 //
-//  This software is licensed under the terms of the GNU General Public License.
-//  http://www.gnu.org/licenses/
-
-//NOTE, adjust code when double digit model numbers come out
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 
 #import "GBDeviceInfo.h"
 
@@ -18,6 +24,7 @@
 @implementation GBDeviceInfo
 
 +(GBDeviceDetails)deviceDetails {
+    //NOTE: adjust code when double digit model numbers come out
     GBDeviceDetails details;
     
     struct utsname systemInfo;
